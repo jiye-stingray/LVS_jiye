2152f539e1f0ad470e3a08427265ed82eac72904
jiye-stingray <dev.jiye@gmail.com>
2026-04-28 22:37:00 +0900

    ✨ l monster damage

--- LVS_jiye/Assets/Scripts/Monster/MonsterBase.cs
+++ LVS_jiye/Assets/Scripts/Monster/MonsterBase.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterBase : UnitBase
@@ -6,11 +7,16 @@ public class MonsterBase : UnitBase
 
     public MonsterData Data => _data;
 
+    private Rigidbody2D _rb;
+
     protected override void Awake()
     {
         if (_data != null)
             _stat = _data.BaseStat;
         base.Awake();
+        _rb = GetComponent<Rigidbody2D>();
+        if (_rb != null)
+            _rb.sleepMode = RigidbodySleepMode2D.NeverSleep;
     }
 
     // 오브젝트 풀에서 꺼낼 때 HP 초기화
@@ -24,5 +30,19 @@ public class MonsterBase : UnitBase
         // TODO: 경험치 지급 (_data.ExpDrop)
         string poolKey = _data != null ? _data.PoolKey : gameObject.name;
         Manager.Instance.Pool.Return(poolKey, gameObject);
+    }
+
+    private readonly Dictionary<Collider2D, float> _hitCooldowns = new();
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        if (_hitCooldowns.TryGetValue(other, out float lastHitTime))
+            if (Time.time - lastHitTime < 0.2f) return;
+
+        _hitCooldowns[other] = Time.time;
+
+        if (other.TryGetComponent<IDamageable>(out var damageable))
+            damageable.TakeDamage(_data.BaseStat.AttackPower);
     }
 }
